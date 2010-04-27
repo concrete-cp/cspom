0f4bad915d74e20199c282b8560e3b002127db8a
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2010-04-27 15:54:23 +0000

    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/cspom@477 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

--- src/main/java/cspom/compiler/CliqueDetector.java
+++ src/main/java/cspom/compiler/CliqueDetector.java
@@ -8,47 +8,56 @@ import cspom.variable.CSPOMVariable;
 
 public final class CliqueDetector {
 
-    private CliqueDetector() {
-    }
+	private CliqueDetector() {
+	}
 
-    public static boolean haveSubsumingConstraint(
-            final CSPOMConstraint constraint,
-            final ConstraintSelector validator) {
-        for (CSPOMConstraint c : constraint.getScope().get(0).getConstraints()) {
-            if (c != constraint && validator.is(c) && subsumes(c, constraint)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public static boolean haveSubsumingConstraint(
+			final CSPOMConstraint constraint, final ConstraintSelector validator) {
+		for (CSPOMConstraint c : constraint.getScope().get(0).getConstraints()) {
+			if (c != constraint && validator.is(c) && subsumes(c, constraint)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public static boolean subsumes(final CSPOMConstraint allDiff,
-            final CSPOMConstraint constraint) {
-        return allDiff.involvesAll(constraint.getScope());
-    }
+	public static boolean subsumes(final CSPOMConstraint allDiff,
+			final CSPOMConstraint constraint) {
+		return allDiff.involvesAll(constraint.getScope());
+	}
 
-    public static boolean allEdges(final CSPOMVariable var,
-            final Collection<CSPOMVariable> vars,
-            final ConstraintSelector validator) {
-        for (CSPOMVariable v : vars) {
-            if (!edge(v, var, validator)) {
-                return false;
-            }
-        }
-        return true;
-    }
+	public static boolean allEdges(final CSPOMVariable var,
+			final Collection<CSPOMVariable> vars,
+			final ConstraintSelector validator) {
+		for (CSPOMVariable v : vars) {
+			if (!edge(v, var, validator)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    private static boolean edge(final CSPOMVariable var1,
-            final CSPOMVariable var2, final ConstraintSelector validator) {
-        final Set<CSPOMConstraint> constraints = var1.getConstraints();
+	private static boolean edge(final CSPOMVariable var1,
+			final CSPOMVariable var2, final ConstraintSelector validator) {
+		if (var2.getConstraints().size() < var1.getConstraints().size()) {
+			final Set<CSPOMConstraint> constraints = var1.getConstraints();
 
-        for (CSPOMConstraint c : var2.getConstraints()) {
-            if (validator.is(c) && constraints.contains(c)) {
-                return true;
-            }
-        }
+			for (CSPOMConstraint c : var2.getConstraints()) {
+				if (validator.is(c) && constraints.contains(c)) {
+					return true;
+				}
+			}
+		} else {
+			final Set<CSPOMConstraint> constraints = var2.getConstraints();
 
-        return false;
-    }
+			for (CSPOMConstraint c : var1.getConstraints()) {
+				if (validator.is(c) && constraints.contains(c)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
 
 }
--- src/main/java/cspom/constraint/AbstractConstraint.java
+++ src/main/java/cspom/constraint/AbstractConstraint.java
@@ -24,111 +24,117 @@ import cspom.variable.CSPOMVariable;
  */
 public abstract class AbstractConstraint implements CSPOMConstraint {
 
-    private final String name;
+	private final String name;
 
-    private final List<CSPOMVariable> scope;
+	private final List<CSPOMVariable> scope;
 
-    private final int arity;
+	private final int arity;
 
-    private final Map<CSPOMVariable, Integer> positions;
+	private final Map<CSPOMVariable, Integer> positions;
 
-    private final String description;
+	private final String description;
 
-    private final String parameters;
+	private final String parameters;
 
-    public AbstractConstraint(final String description,
-            final String parameters, final CSPOMVariable... scope) {
-        this(null, description, parameters, scope);
-    }
+	public AbstractConstraint(final String description,
+			final String parameters, final CSPOMVariable... scope) {
+		this(null, description, parameters, scope);
+	}
 
-    public AbstractConstraint(final String name, final String description,
-            final String parameters, final CSPOMVariable... scope) {
-        this.scope = Arrays.asList(scope);
-        this.name = name;
-        this.description = description;
-        this.parameters = parameters;
-        arity = scope.length;
-        positions = new HashMap<CSPOMVariable, Integer>(arity);
-        for (int i = arity; --i >= 0;) {
-            positions.put(scope[i], i);
-        }
-    }
+	public AbstractConstraint(final String name, final String description,
+			final String parameters, final CSPOMVariable... scope) {
+		this.scope = Arrays.asList(scope);
+		this.name = name;
+		this.description = description;
+		this.parameters = parameters;
+		arity = scope.length;
+		positions = new HashMap<CSPOMVariable, Integer>(arity);
+		for (int i = arity; --i >= 0;) {
+			positions.put(scope[i], i);
+		}
+	}
 
-    @Override
-    public final List<CSPOMVariable> getScope() {
-        return scope;
-    }
+	@Override
+	public final List<CSPOMVariable> getScope() {
+		return scope;
+	}
 
-    @Override
-    public final int getArity() {
-        return arity;
-    }
+	@Override
+	public final int getArity() {
+		return arity;
+	}
 
-    @Override
-    public final Integer getPosition(final CSPOMVariable variable) {
-        return positions.get(variable);
-    }
+	@Override
+	public final Integer getPosition(final CSPOMVariable variable) {
+		return positions.get(variable);
+	}
 
-    @Override
-    public final boolean involves(final CSPOMVariable variable) {
-        return positions.containsKey(variable);
-    }
+	@Override
+	public final boolean involves(final CSPOMVariable variable) {
+		return positions.containsKey(variable);
+	}
 
-    @Override
-    public final boolean involvesAll(final Collection<CSPOMVariable> variables) {
-        return positions.keySet().containsAll(variables);
-    }
+	@Override
+	public final boolean involvesAll(final Collection<CSPOMVariable> variables) {
+		return positions.keySet().containsAll(variables);
+	}
 
-    @Override
-    public int hashCode() {
-        return scope.hashCode() + 31 * getDescription().hashCode();
-    }
+	private int hashCode = 0;
 
-    @Override
-    public final String getDescription() {
-        return description;
-    }
+	@Override
+	public int hashCode() {
+		if (hashCode == 0) {
+			hashCode = scope.hashCode() + 31 * getDescription().hashCode();
+		}
+		return hashCode;
+	}
 
-    @Override
-    public final String getName() {
-        return name;
-    }
+	@Override
+	public final String getDescription() {
+		return description;
+	}
 
-    @Override
-    public final CSPOMVariable getVariable(final int position) {
-        return scope.get(position);
-    }
+	@Override
+	public final String getName() {
+		return name;
+	}
 
-    @Override
-    public boolean equals(final Object object) {
-        if (!(object instanceof AbstractConstraint)) {
-            return false;
-        }
-        final AbstractConstraint constraint = (AbstractConstraint) object;
-        return scope.equals(constraint.scope)
-                && description.equals(constraint.description);
-    }
+	@Override
+	public final CSPOMVariable getVariable(final int position) {
+		return scope.get(position);
+	}
 
-    @Override
-    public final Iterator<CSPOMVariable> iterator() {
-        return scope.iterator();
-    }
+	@Override
+	public boolean equals(final Object object) {
+		if (!(object instanceof AbstractConstraint)) {
+			return false;
+		}
+		final AbstractConstraint constraint = (AbstractConstraint) object;
+		return scope.equals(constraint.scope)
+				&& description.equals(constraint.description);
+	}
 
-    @Override
-    public void replaceVar(final CSPOMVariable merged, final CSPOMVariable var) {
-        int pos = scope.indexOf(merged);
-        if (pos < 0) {
-            throw new IllegalArgumentException(merged + " not in scope");
-        }
-        do {
-            scope.set(pos, var);
-            positions.remove(merged);
-            positions.put(var, pos);
-            pos = scope.indexOf(merged);
-        } while (pos >= 0);
-    }
+	@Override
+	public final Iterator<CSPOMVariable> iterator() {
+		return scope.iterator();
+	}
 
-    public final String getParameters() {
-        return parameters;
-    }
+	@Override
+	public void replaceVar(final CSPOMVariable merged, final CSPOMVariable var) {
+		int pos = scope.indexOf(merged);
+		if (pos < 0) {
+			throw new IllegalArgumentException(merged + " not in scope");
+		}
+		do {
+			scope.set(pos, var);
+			positions.remove(merged);
+			positions.put(var, pos);
+			pos = scope.indexOf(merged);
+		} while (pos >= 0);
+		hashCode = 0;
+	}
+
+	public final String getParameters() {
+		return parameters;
+	}
 }
