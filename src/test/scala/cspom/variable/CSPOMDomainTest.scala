7bedf497d15a1fa9f7c51bbaffa4b71c9e45f247
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2011-05-11 09:57:29 +0000

    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/cspom@615 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

--- src/test/scala/cspom/variable/CSPOMDomainTest.scala
+++ src/test/scala/cspom/variable/CSPOMDomainTest.scala
@@ -1,17 +1,14 @@
 package cspom.variable
 
-import org.junit.Test
 import org.junit.Assert._
+import org.junit.Test
 class CSPOMDomainTest {
   @Test
-  def test() {
-    val domain = CSPOMDomain.valueOf("1 3..10 18..20")
-    assertEquals(ExtensiveDomain.of(1, 3, 4, 5, 6, 7, 8, 9, 10, 18, 19, 20), domain)
-  }
-
-  @Test
-  def testInt() {
-    val domain = CSPOMDomain.valueOf("3..10")
-    assertEquals(new IntInterval(3, 10), domain)
+  def testValueOf() {
+    assertEquals(
+      ExtensiveDomain.of(1, 3, 4, 5, 6, 7, 8, 9, 10, 18, 19, 20),
+      CSPOMDomain.valueOf("1 3..10 18..20"))
+    assertEquals(new IntInterval(3, 10), CSPOMDomain.valueOf("3..10"))
+    assertEquals(new IntInterval(3, 10), CSPOMDomain.valueOf("3..5 6 7..10"))
   }
 }
