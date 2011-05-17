8b4de04ef1cb3200fdc3ba142b6cafe9a8ecd52b
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2011-05-17 09:59:37 +0000

    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/cspom@622 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

---
+++ src/test/scala/cspom/compiler/CompileTest.scala
@@ -0,0 +1,19 @@
+package cspom.compiler;
+
+import cspom.xcsp.{ParserTest, XCSPParser}
+import cspom.CSPOM
+import org.junit.Test
+
+final class CompileTest {
+  val FILENAME = "crossword-m1-debug-05-01.xml";
+
+  @Test
+  def test() {
+    val cspom = new CSPOM();
+    new XCSPParser(cspom).parse(classOf[ParserTest].getResourceAsStream(FILENAME));
+    ProblemCompiler.compile(cspom)
+    println(cspom);
+
+  }
+
+}
