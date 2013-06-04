ea8727c39e70787f9a7f04ff62290dcf2257e79f
Julien Vion <julien.vion@univ-valenciennes.fr>
2013-06-04 15:07:48 +0200

    Moved Problem JSR331 implementation from CSPOM to Concrete

--- src/main/scala/javax/constraints/impl/Constraint.scala
+++
@@ -1,7 +0,0 @@
-package javax.constraints.impl
-
-import cspom.constraint.CSPOMConstraint
-
-class Constraint(problem: Problem, constraint: CSPOMConstraint) extends AbstractConstraint(problem) {
-  setImpl(constraint)
-}
