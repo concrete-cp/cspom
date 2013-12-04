1ccff5862a4ded4e76d57c855fad99274352e5c2
Julien Vion <julien.vion@univ-valenciennes.fr>
2013-12-04 16:58:44 +0100

    Moved declaration of all Expression types in CSPOMExpression to allow the use of sealed traits.

--- src/main/scala/cspom/extension/LazyRelation.scala
+++ src/main/scala/cspom/extension/LazyRelation.scala
@@ -20,48 +20,52 @@ final class LazyRelation(private val text: Reader[Char], val arity: Int, overrid
     def wholeNumber: Parser[Int] = """-?\d+""".r ^^ (_.toInt)
   }
 
-  private def traversable = new Traversable[Array[Int]] {
-    def foreach[A](f: Array[Int] => A) {
-      //text.reset()
-      val seq = Parser.parse(text).get
-      require(seq.size == size, seq.size + " parsed, " + size + " declared")
-      seq.foreach(f)
-    }
-    //      text.reset()
-    //      val buf = new Array[Char](bl)
-    //      var nt = 0
-    //      var b = new StringBuilder()
-    //      while (true) {
-    //        val l = text.read(buf, 0, bl)
-    //        if (l < 0) {
-    //          if (b.nonEmpty) {
-    //            f(b.toString.trim.split(" +").map(_.toInt))
-    //            nt += 1
-    //          }
-    //          require(nt == nbTuples, nt + " parsed, " + nbTuples + " declared")
-    //          return
-    //        }
-    //
-    //        var i = 0
-    //        while (i < l) {
-    //          if (buf(i) == '|') {
-    //            f(b.toString().trim.split(" +").map(_.toInt))
-    //            nt += 1
-    //            b.delete(0, b.length)
-    //          } else {
-    //            b.append(buf(i))
-    //          }
-    //          i += 1
-    //        }
-    //      }
-    //
-    //    }
-  }
+//  private def traversable = new Traversable[Array[Int]] {
+//    def foreach[A](f: Array[Int] => A) {
+//      //text.reset()
+//      val seq = Parser.parse(text).get
+//      require(seq.size == size, seq.size + " parsed, " + size + " declared")
+//      seq.foreach(f)
+//    }
+//    //      text.reset()
+//    //      val buf = new Array[Char](bl)
+//    //      var nt = 0
+//    //      var b = new StringBuilder()
+//    //      while (true) {
+//    //        val l = text.read(buf, 0, bl)
+//    //        if (l < 0) {
+//    //          if (b.nonEmpty) {
+//    //            f(b.toString.trim.split(" +").map(_.toInt))
+//    //            nt += 1
+//    //          }
+//    //          require(nt == nbTuples, nt + " parsed, " + nbTuples + " declared")
+//    //          return
+//    //        }
+//    //
+//    //        var i = 0
+//    //        while (i < l) {
+//    //          if (buf(i) == '|') {
+//    //            f(b.toString().trim.split(" +").map(_.toInt))
+//    //            nt += 1
+//    //            b.delete(0, b.length)
+//    //          } else {
+//    //            b.append(buf(i))
+//    //          }
+//    //          i += 1
+//    //        }
+//    //      }
+//    //
+//    //    }
+//  }
 
   val id = LazyRelation.id
   LazyRelation.id += 1
 
-  def iterator = traversable.toStream.iterator
+  def iterator = {
+    val seq = Parser.parse(text).get
+    require(seq.size == size, seq.size + " parsed, " + size + " declared")
+    seq.iterator
+  }
 
   override def toString = s"relation #${id} with $size tuples"
 
--- src/main/scala/cspom/variable/CSPOMConstant.scala
+++ src/main/scala/cspom/variable/CSPOMConstant.scala
@@ -2,25 +2,12 @@ package cspom.variable
 
 import scala.collection.mutable.WeakHashMap
 
-trait CSPOMConstant extends SimpleExpression {
-  def flattenVariables = Seq()
-  final def replaceVar(which: CSPOMVariable, by: CSPOMExpression) = this
-  def contains(that: CSPOMConstant) = this == that
-  def intersected(that: CSPOMExpression) =
-    if (that.contains(this)) {
-      this
-    } else {
-      throw new IllegalArgumentException("Empty intersection")
-    }
-}
-
 final class IntConstant private (val value: Int) extends CSPOMConstant with IntExpression {
   override def toString = value.toString
   override def equals(o: Any) = o match {
     case i: IntConstant => i.value == value
     case _ => false
   }
-  def cspomType = CSPOMInt
 }
 
 object IntConstant {
@@ -29,17 +16,11 @@ object IntConstant {
   def apply(value: Int) =
     cache.getOrElseUpdate(value, new IntConstant(value))
 
-  //  def unapply(c: CSPOMExpression): Option[Int] = c match {
-  //    case c: IntConstant => Some(c.value)
-  //    case _ => None
-  //  }
-
   def unapply(c: IntConstant): Option[Int] = Some(c.value)
 }
 
 final class DoubleConstant private (val value: Double) extends CSPOMConstant {
   override def toString = value.toString
-  def cspomType = CSPOMDouble
 }
 
 object DoubleConstant {
@@ -49,16 +30,14 @@ object DoubleConstant {
 
 }
 
-object CSPOMTrue extends CSPOMConstant with CSPOMType with BoolExpression {
+object CSPOMTrue extends CSPOMConstant with BoolExpression {
   override def toString = "true"
   def cspomType = this
-  def generalizes(other: CSPOMType) = other == CSPOMTrue
   def neg = CSPOMFalse
 }
 
-object CSPOMFalse extends CSPOMConstant with CSPOMType with BoolExpression {
+object CSPOMFalse extends CSPOMConstant with BoolExpression {
   override def toString = "false"
   def cspomType = this
-  def generalizes(other: CSPOMType) = other == CSPOMFalse
   def neg = CSPOMTrue
 }
