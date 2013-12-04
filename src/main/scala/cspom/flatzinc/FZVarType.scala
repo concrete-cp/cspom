e3e6b482318c336546fab8b233d54b09b83a3abd
Julien Vion <julien.vion@univ-valenciennes.fr>
2013-12-04 16:57:42 +0100

    Removed support of CSPOMType

--- src/main/scala/cspom/flatzinc/FZVarType.scala
+++ src/main/scala/cspom/flatzinc/FZVarType.scala
@@ -1,56 +1,43 @@
 package cspom.flatzinc
 
 import cspom.variable.BoolVariable
-import cspom.variable.CSPOMBool
 import cspom.variable.CSPOMExpression
 import cspom.variable.CSPOMSeq
-import cspom.variable.CSPOMType
 import cspom.variable.CSPOMVariable
-import cspom.variable.CSPOMDouble
-import cspom.variable.CSPOMInt
-import cspom.variable.CSPOMSeqType
 
 sealed trait FZVarType {
   def genVariable(name: String, ann: Set[String]): CSPOMExpression
-  def cspomType: CSPOMType
 }
 
 object FZBoolean extends FZVarType {
   def genVariable(name: String, ann: Set[String]) = new BoolVariable(name, ann)
-  def cspomType = CSPOMBool
 }
 
 object FZFloat extends FZVarType {
   def genVariable(name: String, ann: Set[String]) = ???
-  def cspomType = CSPOMDouble
 }
 
 final case class FZFloatInterval(lb: Double, ub: Double) extends FZVarType {
   def genVariable(name: String, ann: Set[String]) = ???
-  def cspomType = CSPOMDouble
 }
 
 object FZInt extends FZVarType {
   def genVariable(name: String, ann: Set[String]) = ???
-  def cspomType = CSPOMInt
 }
 
 final case class FZIntInterval(lb: Int, ub: Int) extends FZVarType {
   def genVariable(name: String, ann: Set[String]) = CSPOMVariable.ofInterval(name, lb, ub)
-  def cspomType = CSPOMInt
 }
 
 final case class FZIntSeq(values: Seq[Int]) extends FZVarType {
   def genVariable(name: String, ann: Set[String]) = CSPOMVariable.ofIntSeq(name, values, ann)
-  def cspomType = CSPOMInt
 }
 
 final case class FZArray(indices: Range, typ: FZVarType) extends FZVarType {
   def genVariable(name: String, ann: Set[String]) = new CSPOMSeq(
     name,
-    typ.cspomType,
     indices.map(i => typ.genVariable(s"$name[$i]", Set())),
     indices,
     ann)
-  def cspomType = CSPOMSeqType(typ.cspomType)
+
 }
