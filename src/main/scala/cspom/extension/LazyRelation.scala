8d4fa9b67d3175ccefc8875162aac5c179182a0a
Julien Vion <julien.vion@univ-valenciennes.fr>
2014-01-06 13:22:43 +0100

    Parser combinators were too slow for parsing relations, replaced with good old StringTokenizer

--- src/main/scala/cspom/extension/LazyRelation.scala
+++ src/main/scala/cspom/extension/LazyRelation.scala
@@ -7,12 +7,13 @@ import java.io.StringReader
 import scala.util.parsing.combinator.RegexParsers
 import scala.util.parsing.input.CharSequenceReader
 import scala.util.parsing.input.Reader
+import java.io.BufferedReader
 
 object LazyRelation {
   var id = 0
 }
 
-final class LazyRelation(private val text: Reader[Char], val arity: Int, override val size: Int) extends Relation with Loggable {
+final class LazyRelation(private val reader: Reader[Char], val arity: Int, override val size: Int) extends Relation with Loggable {
 
   private object Parser extends RegexParsers {
     def parse: Parser[Seq[Array[Int]]] = repsep(tuple, "|")
@@ -23,11 +24,11 @@ final class LazyRelation(private val text: Reader[Char], val arity: Int, overrid
   val id = LazyRelation.id
   LazyRelation.id += 1
 
-  def iterator = {
-    val seq = Parser.parse(text).get
-    require(seq.size == size, seq.size + " parsed, " + size + " declared")
-    seq.iterator
-  }
+  //  def iterator = {
+  //    val seq = text.toString.split("|").map(_.split(" ").map(_.toInt)) //Parser.parse(text).get
+  //    require(seq.size == size, seq.size + " parsed, " + size + " declared")
+  //    seq.iterator
+  //  }
 
   override def toString = s"relation #${id} with $size tuples"
 
@@ -39,24 +40,27 @@ final class LazyRelation(private val text: Reader[Char], val arity: Int, overrid
   }
 
   //  
-  //  new Iterator[Array[Int]] {
-  //    val st = new StringTokenizer(text, "|")
-  //    var c = 0
-  //    def hasNext = {
-  //      if (st.hasMoreTokens) true
-  //      else {
-  //        require(c == nbTuples)
-  //        text = null
-  //        false
-  //      }
-  //    }
-  //    def next() = {
-  //      c += 1
-  //      val t = st.nextToken().trim.split(" +")
-  //      require(t.length == arity, t.toSeq.toString)
-  //      t.map(_.toInt)
-  //    }
-  //  }
+
+  @annotation.tailrec
+  private def readerToString(r: Reader[Char], stb: StringBuilder = new StringBuilder): String =
+    if (r.atEnd) {
+      stb.toString
+    } else {
+      readerToString(r.rest, stb.append(r.first))
+    }
+
+  def iterator = new Iterator[Array[Int]] {
+    val text = readerToString(reader)
+
+    val st = new StringTokenizer(text, "|")
+
+    def hasNext = st.hasMoreTokens
+    def next() = {
+      val t = st.nextToken().trim.split(" +")
+      require(t.length == arity, t.toSeq.toString)
+      t.map(_.toInt)
+    }
+  }
 
   def contains(t: Seq[Int]) = exists(_ sameElements t)
 
