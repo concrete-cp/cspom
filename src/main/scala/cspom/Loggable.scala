514590d182090b503767fbd3a28bd6c25eb78622
Julien Vion <julien.vion@univ-valenciennes.fr>
2014-02-17 10:47:44 +0100

    Logging facilities moved to CSPOM, improved API

--- src/main/scala/cspom/Loggable.scala
+++ src/main/scala/cspom/Loggable.scala
@@ -2,6 +2,9 @@ package cspom
 
 import java.util.logging.Logger
 import java.util.logging.Level
+import java.io.PrintStream
+import java.util.logging.LogRecord
+import java.util.logging.Handler
 trait Loggable {
 
   val logger: Logger = Logging.getLogger(this)
@@ -14,11 +17,42 @@ trait Loggable {
     logger.setLevel(level)
   }
 }
+final class MsLogHandler(val start: Long = System.currentTimeMillis(), val out: PrintStream = System.err) extends Handler {
+  def close() {
+    out.close();
+  }
 
+  def flush() {
+    out.flush();
+  }
+
+  def publish(arg0: LogRecord) {
+    if (isLoggable(arg0)) {
+      out.println("%% [%8.3f] %7s : %s (%s.%s)".format(
+        (arg0.getMillis() - start) / 1e3d, arg0.getLevel(),
+        arg0.getMessage(), arg0.getSourceClassName(),
+        arg0.getSourceMethodName()));
+    }
+  }
+
+}
 /**
  * Note: implementation taken from scalax.logging API
  */
 object Logging {
+
+  for (h <- Logger.getLogger("").getHandlers()) {
+    Logger.getLogger("").removeHandler(h);
+  }
+
+  val handler = new MsLogHandler(System.currentTimeMillis());
+  Logger.getLogger("").addHandler(handler);
+
+  def setLevel(l: Level) {
+    handler.setLevel(l)
+    Logger.getLogger("").setLevel(l);
+  }
+
   def loggerNameForClass(className: String) = {
     if (className endsWith "$") className.substring(0, className.length - 1)
     else className
