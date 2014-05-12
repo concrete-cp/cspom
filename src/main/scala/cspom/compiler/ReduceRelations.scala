2e548a7d41475729bc7213e5f9e8218726e24c9a
Julien Vion <julien.vion@univ-valenciennes.fr>
2014-05-12 18:25:42 +0200

    Added a cache to avoid copied relations

--- src/main/scala/cspom/compiler/ReduceRelations.scala
+++ src/main/scala/cspom/compiler/ReduceRelations.scala
@@ -7,11 +7,16 @@ import cspom.variable.CSPOMConstant
 import cspom.variable.CSPOMVariable
 import cspom.variable.SimpleExpression
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import scala.collection.mutable.HashMap
+import cspom.extension.MDD
+import scala.collection.mutable.WeakHashMap
 
 /**
  * Detects and removes constants from extensional constraints
  */
 object ReduceRelations extends ConstraintCompilerNoData with LazyLogging {
+
+  val cache = new WeakHashMap[Relation[_], Relation[_]]
 
   override def matchBool(c: CSPOMConstraint[_], problem: CSPOM) = c.function == 'extension && c.nonReified
 
@@ -32,10 +37,12 @@ object ReduceRelations extends ConstraintCompilerNoData with LazyLogging {
       case (c: CSPOMVariable[_], i) => i
     }
     val projected = if (vars.size < c.arguments.size) { filtered.project(vars) } else { filtered }
+    
+    val cached = cache.getOrElseUpdate(projected, projected)
 
-    logger.info(relation + " -> " + projected)
+    logger.info(relation + " -> " + cached)
     replaceCtr(c,
-      CSPOMConstraint('extension, vars.map(args), c.params.updated("relation", projected)),
+      CSPOMConstraint('extension, vars.map(args), c.params.updated("relation", cached)),
       problem)
 
   }
