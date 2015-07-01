564c3f8132d5eb21f80711d658cadf6d80488b64
Julien Vion <julien.vion@univ-valenciennes.fr>
2015-07-01 18:31:39 +0200

    Moved matrix macros to Table object

--- src/main/scala/cspom/extension/Table.scala
+++ src/main/scala/cspom/extension/Table.scala
@@ -1,6 +1,10 @@
 package cspom.extension
 
 import scala.collection.Seq
+import language.experimental.macros
+
+import scala.reflect.macros.blackbox.Context
+import scala.util.Try
 
 class Table[A](val table: Set[Seq[A]]) extends Relation[A] {
 
@@ -25,7 +29,48 @@ class Table[A](val table: Set[Seq[A]]) extends Relation[A] {
 
   override def equals(o: Any) = o match {
     case t: Table[A] => t.table == table
-    case o => super.equals(o)
+    case o           => super.equals(o)
 
   }
+
+}
+
+object Table {
+
+  implicit class MatrixContext(sc: StringContext) {
+    def matrix(): Array[Array[Int]] = macro matrixImpl
+  }
+
+  def matrixImpl(c: Context)(): c.Expr[Array[Array[Int]]] = {
+    import c.universe.{ Try => _, _ }
+
+    val matrix = Try {
+      c.prefix.tree match {
+        case Apply(_, List(Apply(_, List(Literal(Constant(raw: String)))))) =>
+
+          def toArrayAST(c: List[TermTree]): Apply =
+            Apply(Select(Select(Ident(TermName("scala")), TermName("Array")), TermName("apply")), c)
+
+          val matrix = raw
+            .split("\n")
+            .map(_.trim)
+            .filter(_.nonEmpty)
+            .map {
+              _.split(",").map(_.trim.toInt)
+            }
+          if (matrix.map(_.length).distinct.size != 1) {
+            c.abort(c.enclosingPosition, "rows of matrix do not have the same length")
+          }
+
+          val matrixAST = matrix
+            .map(_.map(i => Literal(Constant(i))))
+            .map(i => toArrayAST(i.toList))
+
+          toArrayAST(matrixAST.toList)
+      }
+    }
+
+    c.Expr(matrix.getOrElse(c.abort(c.enclosingPosition, "not a matrix of Int")))
+  }
+
 }
