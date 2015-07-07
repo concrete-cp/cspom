491c8277aa74117188d9624549f06dbb6579181a
Julien Vion <julien.vion@univ-valenciennes.fr>
2015-07-07 17:33:19 +0200

    Moved BitVector data structures from Concrete as they are used for CSPOMCompiler

---
+++ src/main/scala/cspom/util/BitVector.scala
@@ -0,0 +1,208 @@
+package cspom.util
+
+import BitVector._
+import java.util.Arrays
+
+final object BitVector {
+  private val ADDRESS_BITS_PER_WORD = 6
+  val WORD_SIZE = 1 << ADDRESS_BITS_PER_WORD
+  val MASK = 0xFFFFFFFFFFFFFFFFL
+
+  val empty: BitVector = EmptyBitVector
+
+  def filled(size: Int) = empty.set(0, size)
+
+  //  def nbWords(nbBits: Int) = {
+  //    if (nbBits % WORD_SIZE > 0) {
+  //      word(nbBits) + 1
+  //    } else {
+  //      word(nbBits)
+  //    }
+  //  }
+
+  def word(bit: Int) = bit >> ADDRESS_BITS_PER_WORD
+
+  def apply(v: Traversable[Int]) = {
+    empty ++ v
+  }
+}
+
+trait BitVector extends Any {
+
+  def iterator = new Iterator[Int] {
+    var current = nextSetBit(0)
+    def hasNext = current >= 0
+    def next() = {
+      val c = current
+      current = nextSetBit(current + 1)
+      c
+    }
+  }
+
+  override def toString(): String = iterator.mkString("{", ", ", "}")
+
+  def set(position: Int, status: Boolean) = {
+    if (status) {
+      this + position
+    } else {
+      this - position
+    }
+  }
+
+  def set(from: Int, until: Int): BitVector = {
+    if (from >= until) {
+      this
+    } else {
+      val startWordIndex = word(from)
+      val maskFrom = MASK << from
+      val lastWordIndex = word(until - 1)
+      val maskTo = MASK >>> -until
+
+      val newWords = getWords.padTo(lastWordIndex + 1, 0L)
+      val sw = newWords(startWordIndex)
+
+      var changed = false
+      if (startWordIndex == lastWordIndex) {
+        newWords(startWordIndex) |= (maskFrom & maskTo)
+      } else {
+        newWords(startWordIndex) |= maskFrom
+
+        val lw = newWords(lastWordIndex)
+        newWords(lastWordIndex) |= maskTo
+
+        changed |= (lw != newWords(lastWordIndex))
+
+        for (i <- startWordIndex + 1 until lastWordIndex) {
+          if (newWords(i) != MASK) {
+            newWords(i) = MASK
+            changed = true
+          }
+        }
+
+      }
+      changed |= (sw != newWords(startWordIndex))
+
+      if (changed) {
+        if (newWords.length == 1) {
+          new SmallBitVector(newWords.head)
+        } else {
+          new LargeBitVector(newWords)
+        }
+      } else {
+        this
+      }
+    }
+  }
+
+  def getWords: Array[Long]
+
+  def -(position: Int): BitVector
+
+  def +(position: Int): BitVector = {
+    val wordPos = word(position)
+    val oldWord = getWord(wordPos)
+    val newWord = oldWord | (1L << position)
+
+    if (oldWord == newWord) {
+      this
+    } else {
+      setWord(wordPos, newWord)
+    }
+  }
+
+  def ++(p: Traversable[Int]): BitVector = {
+    var words = getWords
+    var change = false
+
+    for (i <- p) {
+      val wordPos = word(i)
+      if (wordPos >= words.length) {
+        words = Arrays.copyOf(words, wordPos + 1)
+      }
+      val oldWord = words(wordPos)
+      val newWord = oldWord | (1L << i)
+      if (oldWord != newWord) {
+        change = true
+        words(wordPos) = newWord
+      }
+    }
+
+    if (change) {
+      words.size match {
+        case 0 => EmptyBitVector
+        case 1 => new SmallBitVector(words(0))
+        case _ => new LargeBitVector(words)
+      }
+    } else {
+      this
+    }
+  }
+  def --(p: Traversable[Int]) = p.foldLeft(this)(_ - _)
+
+  def apply(position: Int): Boolean
+
+  def nextSetBit(start: Int): Int
+
+  def prevSetBit(start: Int): Int
+
+  def lastSetBit: Int
+
+  def clearFrom(from: Int): BitVector
+
+  def clearUntil(until: Int): BitVector
+
+  def intersects(bV: BitVector, position: Int): Boolean
+
+  def intersects(bV: BitVector): Int;
+
+  def nbWords: Int
+
+  def ^(bv: BitVector): BitVector
+
+  def &(bv: BitVector): BitVector
+
+  def |(bv: BitVector): BitVector
+
+  def isEmpty: Boolean
+
+  def cardinality: Int
+
+  def getWord(i: Int): Long
+
+  def subsetOf(bv: BitVector): Boolean
+
+  def setWord(pos: Int, word: Long): BitVector
+
+  def filter(f: Int => Boolean): BitVector
+}
+
+object EmptyBitVector extends BitVector {
+  def &(bv: BitVector) = this
+  def |(bv: BitVector) = bv
+  def -(position: Int) = this
+  def ^(bv: BitVector) = bv
+  def apply(position: Int): Boolean = false
+  def cardinality = 0
+  def clearFrom(from: Int) = this
+  def clearUntil(to: Int) = this
+  def filter(f: Int => Boolean) = this
+  def getWord(i: Int): Long = 0L
+  def getWords: Array[Long] = Array()
+  def intersects(bV: BitVector): Int = -1
+  def intersects(bV: BitVector, position: Int): Boolean = false
+  def isEmpty: Boolean = true
+  def lastSetBit: Int = -1
+  def nbWords: Int = 0
+  def nextSetBit(start: Int): Int = -1
+  def prevSetBit(start: Int): Int = -1
+  def setWord(pos: Int, word: Long) = {
+    if (pos == 0) {
+      new SmallBitVector(word)
+    } else {
+      val array = new Array[Long](pos + 1)
+      array(pos) = word
+      new LargeBitVector(array)
+    }
+  }
+  def subsetOf(bv: BitVector): Boolean = true
+}
