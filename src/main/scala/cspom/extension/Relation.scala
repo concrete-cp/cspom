52545f9878258686bababa3e300aa2d5b02f7e7e
Julien Vion <julien.vion@univ-valenciennes.fr>
2016-09-16 16:37:32 +0200

    Reintroduced matrix macro in Relation but it does not seem to work anymore

--- src/main/scala/cspom/extension/Relation.scala
+++ src/main/scala/cspom/extension/Relation.scala
@@ -1,4 +1,8 @@
 package cspom.extension
+import language.experimental.macros
+
+import scala.reflect.macros.blackbox.Context
+import scala.util.Try
 
 trait Relation[A] extends Iterable[Seq[A]] {
 
@@ -12,4 +16,43 @@ trait Relation[A] extends Iterable[Seq[A]] {
 
   def arity: Int
 
+}
+
+object Relation {
+
+  implicit class MatrixContext(sc: StringContext) {
+    def matrix(): Array[List[Int]] = macro matrixImpl
+  }
+
+  def matrixImpl(c: Context)(): c.Expr[Array[List[Int]]] = {
+    import c.universe.{ Try => _, _ }
+
+    val matrix = Try {
+      c.prefix.tree match {
+        case Apply(_, List(Apply(_, List(Literal(Constant(raw: String)))))) =>
+
+          def toArrayAST(c: List[TermTree]): Apply =
+            Apply(Select(Select(Ident(TermName("scala")), TermName("Array")), TermName("apply")), c)
+
+          val matrix = raw
+            .split("\n")
+            .map(_.trim)
+            .filter(_.nonEmpty)
+            .map {
+              _.split(",").map(_.trim.toInt)
+            }
+          if (matrix.map(_.length).distinct.size != 1) {
+            c.abort(c.enclosingPosition, "rows of matrix do not have the same length")
+          }
+
+          val matrixAST = matrix
+            .map(_.map(i => Literal(Constant(i))))
+            .map(i => toArrayAST(i.toList))
+
+          toArrayAST(matrixAST.toList)
+      }
+    }
+
+    c.Expr(matrix.getOrElse(c.abort(c.enclosingPosition, "not a matrix of Int")))
+  }
 }
